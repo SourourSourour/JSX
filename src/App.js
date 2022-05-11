3bee82244c2a857b06afc980c32f55d60e1351b5
SourourSourour <issaoui.sourour22@gmail.com>
2022-05-11 11:14:37 +0100

    JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import image from "./photo.jpg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        style={{
+          border: "solid 1px black",
+          maxWidth: "100vw",
+        }}
+      >
+        <h1 className="title red">Sourour Issaoui</h1>
+        <br />
+        <img
+          className="imge"
+          src={image}
+          alt="imageInSrc"
+        />
+        <br />
+        <img
+          className="imge"
+          src="/photo1.jpg"
+          alt="imageInPublic"
+         
+        />
+      </div>
+
+      <video
+        className="video"
+        style={{
+          width: "100%",
+          height: "600px",
+        }}
+        controls
+      >
+        <source src="/myVideo.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
